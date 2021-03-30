a868a6bd0dab646c0c8a19ceacf13788aede0e9c
rikima0519 <s.rikima@icloud.com>
2021-03-30 10:23:47 +0900

    アソシエーション、README作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
 
    validates :name, presence: true, length: { maximum: 50 }
 
+   has_many :posts, dependent: :destroy
+
    def update_without_current_password(params, *options)
 
     if params[:password].blank? && params[:password_confirmation].blank?
